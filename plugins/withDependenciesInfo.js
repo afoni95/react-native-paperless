a52528f9c611e767d11084fa0c2320c57faf6f0f
afoni95 <antonios.vamvakos@web.de>
2026-05-27 19:19:57 +0200

    Add dependenciesInfo to build.gradle and create config plugin

---
+++ plugins/withDependenciesInfo.js
@@ -0,0 +1,33 @@
+// @ts-check
+const { withAppBuildGradle } = require('@expo/config-plugins');
+
+/**
+ * Expo config plugin that injects `dependenciesInfo` into the android {} block
+ * of android/app/build.gradle, disabling dependency metadata in APK/bundle.
+ */
+const withDependenciesInfo = (config) => {
+  return withAppBuildGradle(config, (mod) => {
+    let contents = mod.modResults.contents;
+
+    if (contents.includes('dependenciesInfo')) {
+      return mod;
+    }
+
+    const block = `
+    dependenciesInfo {
+        includeInApk = false
+        includeInBundle = false
+    }`;
+
+    // Inject just before the closing brace of the android {} block
+    contents = contents.replace(
+      /(androidResources\s*\{[^}]*\})\s*\n(\})/,
+      `$1\n${block}\n$2`,
+    );
+
+    mod.modResults.contents = contents;
+    return mod;
+  });
+};
+
+module.exports = withDependenciesInfo;
